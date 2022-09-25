cc78fec5522870020559dce2e02f255e57b3206f
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-09-25 22:43:11 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe() {
+  return (
+    <div>
+        <button onFocus={event =>(console.log('Good!'))} 
+        onBlur={event =>(console.log('Hey! Eyes on me!'))}>EyesOnMe</button> 
+    </div>
+  )
+}
+
+export default EyesOnMe
+
